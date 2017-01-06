c928cdcb0d405a53c964102ba5cbb92fe9984ccf
Johnson Tsay <johnsontsay@johnsons-imac.attlocal.net>
2017-01-05 17:04:48 -0800

    setup Firebase

---
+++ Pods/Target Support Files/Pods-twoseconds/Pods-twoseconds-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_twoseconds : NSObject
+@end
+@implementation PodsDummy_Pods_twoseconds
+@end
---
+++ Pods/Target Support Files/Pods-twoseconds/Pods-twoseconds-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_twosecondsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_twosecondsVersionString[];
+
